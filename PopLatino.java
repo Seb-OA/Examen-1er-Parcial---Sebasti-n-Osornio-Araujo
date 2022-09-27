4be10615149704cd351adbf680fe96d1ef8251fd
Seb-OA <113382083+Seb-OA@users.noreply.github.com>
2022-09-26 19:50:33 -0500

    Add files via upload

---
+++ PopLatino.java
@@ -0,0 +1,22 @@
+
+package examen;
+
+/**
+ *
+ * @author Sebastián Osornio Araujo
+ */
+ 
+public class PopLatino extends ClasePadre {   
+
+    @Override
+    public void Recomendacion() {
+        
+        System.out.println("Lasso: Ojos Marrones(2022)");
+        System.out.println("Morat: Balas Perdidas(2018)");
+        System.out.println("Camilo: Por Primera Vez(2020");
+
+    }
+    
+}
+    
+
---
+++ UrbanoLatino.java
@@ -0,0 +1,18 @@
+
+package examen;
+
+/**
+ *
+ * @author Sebastián Osornio Araujo
+ */
+public class UrbanoLatino extends ClasePadre {
+    
+    @Override
+    public void Recomendacion() {
+        
+        System.out.println("Bad Bunny: Un Verano Sin Ti(2022)");
+        System.out.println("J Balvin: Colores(2020");
+        System.out.println("Rauw Alejandro: Todo De Ti(2021");
+    }
+    
+}
